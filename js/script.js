557b321d9af983867cebc3b573ad27c4c2aa73fb
kariambar <karina.spalacios@gmail.com>
2017-05-16 12:45:49 -0400

    primer commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 function agregar(){
+	var tareas = document.getElementById('tarea').value;
+	document.getElementById('tarea').value = "";
+
+	var cont = document.getElementById('contenedor');
 	
 }
 
